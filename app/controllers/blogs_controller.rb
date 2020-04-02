ed0a003015da574552c889854c42248bedab3d4e
tatsuya matsuhashi <maa0917@gmail.com>
2020-04-02 17:55:53 +0900

    feat: blog機能を追加した

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,39 @@
+class BlogsController < ApplicationController
+
+  before_action :set_blog, only: [:show, :destroy]
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def show
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_url
+  end
+
+  private
+
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  def blog_params
+    params.require(:blog).permit(:content)
+  end
+end
