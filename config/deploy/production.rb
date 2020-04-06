dd84181e639b8c6b1e6018ac28b41103688db02a
tatsuya matsuhashi <maa0917@gmail.com>
2020-04-06 19:01:42 +0900

    ruby version修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '18.178.18.180', user: 'app', roles: %w{app db web}
+server '18.178.157.10', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/Users/tatsuyamatsuhashi/.ssh/id_rsa'
