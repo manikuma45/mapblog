c7b38148f7d1148e4ec7520f9e04116400b23800
tatsuya matsuhashi <maa0917@gmail.com>
2020-04-07 16:51:20 +0900

    add: user blog show画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users, controllers: {
-        registrations: 'users/registrations'
+    registrations: 'users/registrations'
   }
   resources :users, only: [:show]
+  get 'users/blog_show'
 
   resources :blogs
   get 'home', to: 'blogs#home'
