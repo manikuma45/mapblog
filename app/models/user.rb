213ff559eed3e897fd88201ae6ee317558a57900
tatsuya matsuhashi <maa0917@gmail.com>
2020-04-07 14:32:57 +0900

    add: usersテーブルにnameカラム追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
+  
   has_many :blogs, dependent: :destroy
 end
