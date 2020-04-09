acd932425a1c2c62c4f5d619b1f73252ed3b90c4
tatsuya matsuhashi <maa0917@gmail.com>
2020-04-09 14:14:54 +0900

    feat: like_blogs画面作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,4 +8,19 @@ class LikesController < ApplicationController
     @like = Like.find(params[:id]).destroy
     @blog = @like.blog
   end
+
+  def like_blogs
+    @user = User.find(params[:user_id])
+    @blogs = @user.like_blogs.order(created_at: "DESC")
+    @markers_json = @blogs.map do |blog|
+      [
+        blog.id,
+        blog.lat,
+        blog.lng,
+        blog.content,
+        blog.image.thumb.url,
+        blog.user.icon.thumb.url,
+      ]
+    end.to_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   resources :likes, only: [:create, :destroy]
+
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  resources :users, only: [:show]
+
+  resources :users, only: [:show] do
+    get 'likes', to: 'likes#like_blogs'
+  end
   get 'users/blog_show'
 
   resources :blogs
